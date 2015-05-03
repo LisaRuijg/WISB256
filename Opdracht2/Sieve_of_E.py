02a3a33feb71d2115269de55e2e4343e75cd919c
lisaruijg <lisaruijg94@hotmail.com>
2015-05-03 12:14:53 +0000

    Verbetering eind versie

--- Opdracht2/Sieve_of_E.py
+++ Opdracht2/Sieve_of_E.py
@@ -6,20 +6,22 @@ bestandsnaam = sys.argv[2]
 T1 = time.perf_counter()
 
 def primes(n):
-    getallen = list(range ( 2, n , 1))
-    for deler in getallen:
-        for x in getallen:
-            if x%deler==0 and x!=deler and deler <= int(round(n**0.5)) :   # all delers less or equal than the square root of n don't have to be considered
-                getallen.remove(x)
+    getallen = list(range ( 0, n))
+    getallen[1]=0
+    for i in range(1,n):
+        if getallen[i]!=0:
+            for j in range(2*i,n,i):
+                getallen[j]=0
     return getallen
     
 T2 = time.perf_counter()
 
 document = open ( bestandsnaam , 'w')
 for x in primes(n):
-    document.write(str(x) +' \n')
+    if x!=0:
+        document.write(str(x) +' \n')
 document.close()
     
-number_of_primes= len(primes(n))
+number_of_primes= sum( 1 for line in open(bestandsnaam))
 
 print('Found ' + str(number_of_primes) + ' Prime numbers smaller than ' + str(n) + ' in ' + str(T2-T1) + ' sec.')
--- Opdracht3/estimate_pi.py
+++ Opdracht3/estimate_pi.py
@@ -1,9 +1,18 @@
 import random
 import sys
+import math
+
 N = 2
 L = 0.5
 #N = int(sys.argv[1])    
 #L = int(sys.argv[2])
+
+x_0 = random.random()
+y_0 = random.random()
+a = random.vonmisesvariate(0,0)
+
+x_1 = x_0 + L*math.cos(a)
+y_1 = y_0 + L*math.sin(a)
 
 def drop_needle(L):
     for i in range(int(N)):
