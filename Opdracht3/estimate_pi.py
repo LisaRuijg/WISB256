b1877048ac48d576c0995fa93c05b122ab409bf3
lisaruijg <lisaruijg94@hotmail.com>
2015-05-07 14:38:31 +0000

    Inlever klaar

--- Opdracht3/estimate_pi.py
+++ Opdracht3/estimate_pi.py
@@ -2,24 +2,41 @@ import random
 import sys
 import math
 
-N = 2
-L = 0.5
-#N = int(sys.argv[1])    
-#L = int(sys.argv[2])
-
-x_0 = random.random()
-y_0 = random.random()
-a = random.vonmisesvariate(0,0)
-
-x_1 = x_0 + L*math.cos(a)
-y_1 = y_0 + L*math.sin(a)
+seed = sys.argv[3]
+random.seed(int(seed))
 
 def drop_needle(L):
-    for i in range(int(N)):
-        N==i
+    x_0 = random.random()   #uniform in [0,1]
+    # y_0 = random.random()
+    a = random.vonmisesvariate(0,0)     #uniform in [0, 2*pi]
+    x_1 = x_0 + L*math.cos(a)
+    # y_1 = y_0 + L*math.sin(a)
+    if x_1 >= 1:
+        return True
+    elif x_1 <= 0:
+        return True
+    else:
+        return False
 
+def hits():
+    if len(sys.argv)<3:
+        print('Use: estimate_pi.py N L')
+        return
+    
+    N = int(sys.argv[1])    
+    L = float(sys.argv[2])
+    
+    if L >1:
+        print('AssertionError: L should be smaller than 1')
+        return
+    
+    count = 0    
+    for i in range(1,N+1):
+        if drop_needle(L)==True:
+            count = count +1
+    print(str(count) + ' hits in '+ str(N) + ' tries')
+    estimate_pi = (2*L*N)/(count)
+    print('Pi = '+ str(estimate_pi))
+    return
 
-h = 3
-print(' hits '+' in '+' tries.')
-estimate_pi = (2*L*N)/(h)
-print('Pi = '+ str(estimate_pi))
+hits()
