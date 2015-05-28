342d51400c16af26dfcbefb615b69f02911e8229
lisaruijg <lisaruijg94@hotmail.com>
2015-05-28 13:34:45 +0000

    Bonus deel erbij

--- Opdracht5/Vector.py
+++ Opdracht5/Vector.py
@@ -8,12 +8,12 @@ class Vector():
             self.vector = [a]*n    
             
     def __str__(self):
-        return "\n".join(str("{0:.6f}".format(x)) for x in self.vector)
+        return "\n".join(str("{0:.6f}".format(x)) for x in self.vector)+'\n'
     
     def scalar(self,alpha=1):
         return Vector(len(self.vector),[alpha * self.vector[i] for i in range(len(self.vector))])
         
-    def lincomb(self,other,alpha,beta):    
+    def lincomb(self,other,alpha=1,beta=1):    
         A = [alpha * self.vector[i] for i in range(len(self.vector))]
         B = [beta * other.vector[i] for i in range(len(other.vector))]
         return Vector(len(self.vector),[A[i]+B[i] for i in range(len(self.vector))])
@@ -27,10 +27,28 @@ class Vector():
         return math.sqrt(kwadraat)
         
     def proj(self,other):
-        lijst_2 =[((self.inner(other))/(self.inner(self)))*self.vector[i] for i in range(len(self.vector))]
-        return math.fsum(lijst_2)
+        lijst_2 =((self.inner(other))/(self.inner(self)))
+        resultaat = self.scalar(lijst_2)
+        return resultaat
     
-    def GrammSchmidt(self,other):
-        u_1 = self.vector
-        u_2 = other.vector
-        
+    def normal(self):
+        return self.scalar(1/self.norm())
+
+def projoptellen(lijst,v):
+    p=[]
+    for i in range(0,len(lijst)):
+        p.append(lijst[i].proj(v[len(lijst)]))
+    waarden=p[0]
+    for i in range(1,len(p)):
+        waarden=waarden.lincomb(p[i])
+    som=waarden.scalar(-1)
+    return som
+
+def GrammSchmidt(v):
+    lijst=[v[0]]
+    for i in range(1,len(v)):
+        lijst.append(v[len(lijst)].lincomb(projoptellen(lijst,v)))
+    e=[]
+    for i in lijst:
+        e.append(i.normal())
+    return e    
